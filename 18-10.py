af67551e3f84444ca32c48831418e0dbbc6f9350
Свистунова Марина Петровна <31745345+MarinaSvistunova@users.noreply.github.com>
2018-10-18 16:37:14 +0300

    4 commit

--- 18-10.py
+++ 18-10.py
@@ -1,8 +1,8 @@
 def main(*lst):
   return sorted(lst)
 
-
-print(main((1, 8, 2, 99, 1.3, 5)))
+a = [int(i) for i in input().split()]
+print(main(*a))
 
 if __name__ == '__main__':
   assert main(1, 8, 2, 99, 1.3, 5) == [1, 1.3, 2, 5, 8, 99],\
