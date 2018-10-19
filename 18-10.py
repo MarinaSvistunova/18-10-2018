cfe424285bacce8f2c3e0a51cc54e3546c56e16d
Свистунова Марина Петровна <31745345+MarinaSvistunova@users.noreply.github.com>
2018-10-19 14:37:09 +0300

    5 commit

--- 18-10.py
+++ 18-10.py
@@ -1,9 +1,33 @@
 def main(*lst):
-  return sorted(lst)
+    lst2 = [0] * len(lst)
+    for k in range(len(lst)):
+        if lst[k].isdigit():
+            lst2[k] = int(lst[k])
+        elif lst[k] == 'True' or lst[k] == 'False':
+            if lst[k] == 'True':
+                lst2[k] = True;
+            elif lst[k] == 'False':
+                lst2[k] = False;
+            lst2[k] = lst2[k]
+        else:
+            try:
+                float(lst[k])
+                lst2[k] = float(lst[k])
+            except ValueError:
+                lst2[k] = lst[k]
+    return sorted(lst2)
 
-a = [int(i) for i in input().split()]
+a = [i for i in input().split(' ')]
 print(main(*a))
 
 if __name__ == '__main__':
-  assert main(1, 8, 2, 99, 1.3, 5) == [1, 1.3, 2, 5, 8, 99],\
-          "Сортировка по возрастанию"
+    assert main('1', '8', '2', '99', '5') == [1, 2, 5, 8, 99],\
+          "Сортировка по возрастанию целых чисел"
+    assert type(main('1', '8', '2', '99', '5')) == list,\
+          "Тип"
+    assert main('1.3', '8.1', '2.1', '1.1', '99.3', '5.4') == [1.1, 1.3, 2.1, 5.4, 8.1, 99.3], \
+        "Сортировка по возрастанию чисел с плавающей запятой"
+    assert main('True', 'False', 'False', 'True') == [False, False, True, True], \
+        "Сортировка по возрастанию значений логических типов"
+    assert main('love', 'with my-myself', 'I am', 'in') == ['I am', 'in', 'love', 'with my-myself'], \
+        "Сортировка по возрастанию строк"
