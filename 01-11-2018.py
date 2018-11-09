536f1f1a120a470c8b131247bef8b09de5d9b2bf
Свистунова Марина Петровна <31745345+MarinaSvistunova@users.noreply.github.com>
2018-11-09 20:45:43 +0300

    7 commit

---
+++ 01-11-2018.py
@@ -0,0 +1,39 @@
+def unique_func(iterable, seen=list()): #seen = None
+    # Функция возвращает список уникальных элементов
+    if len(seen) == 0:   #seen = list(seen or [])
+        seen = list()
+    for item in iterable:
+        if item not in seen:
+            seen.append(item)
+    return list(seen)
+
+print(unique_func([1, 1, 2, 27, 3, 3]))
+print(unique_func([1, 1, 2, 3, 3], seen = [2, 9]))
+
+#print(unique_func([1, 1, 2, 3, 3], seen = unique_func([3, 3, 9])))
+
+
+def unique_func2(*iterable, seen = []):
+    # Функция возвращает список уникальных элементов. В функцию может быть передано различное количество элементов
+    if len(seen) == 0:
+        seen = list()
+    for item in iterable:
+        if type(item) == list:
+            for item2 in item:
+                if item2 not in seen:
+                    seen.append(item2)
+        else:
+            if item not in seen:
+                seen.append(item)
+    return list(seen)
+
+print(unique_func2([1, 1, 2, 27, 3, 3], [6, 1], 7, 's', 'f', seen = [4]))
+
+if __name__ == '__main__':
+    assert type(unique_func2([1, 4, 5, 5, 5, 3, 77, 4], seen =[3, 8])) == list, "Тип"
+    assert unique_func2([1, 4, 5, 5, 5, 3, 77, 4], seen =[3, 8]) == [3, 8, 1, 4, 5, 77], "Целые числа"
+    assert unique_func2([1, 3, 's'], 'd', 6, 5, 7, 's', seen =[3, 8]) == [3, 8, 1, 's', 'd', 6, 5, 7], "Список и произвольные аргументы"
+    assert unique_func2([1, 3, 's'], 'd', 6, 5, 7, 's') == [1, 3, 's', 'd', 6, 5, 7], "Список и произвольные аргументы, без seen"
+    assert unique_func2([1, 4.5, 5, 5, 5, 3, 77, 4], seen =[3, 'g', 8]) == [3, 'g', 8, 1, 4.5, 5, 77, 4], "Два списка"
+    assert unique_func2('f', [1, 4, 5, 5, 5, 3, 4], seen=[3, 8]) == [3, 8, 'f', 1, 4, 5], "Произвольный элемент и список"
+    assert unique_func2('f', seen=[3, 8]) == [3, 8, 'f'], "Произвольный элемент и список"
---
+++ 08-11-2018.py
@@ -0,0 +1,10 @@
+import math
+z = list(map(lambda *x: tuple([i for i in x]), range(1,6), range(5,12), range(11,18), range(1,6)))
+print(z)
+p = [x**2 for x in [1,3,4,15]]
+print(p)
+pt = [1, 4, 5, -6]
+k = [int(math.sqrt(x*x)) for x in pt]
+print(k)
+k2 = [abs(x) for x in pt]
+print(k2)
